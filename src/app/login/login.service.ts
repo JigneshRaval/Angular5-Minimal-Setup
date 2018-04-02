7435a385864885b1299ce3edea9694d8b93d7039
Jignesh Raval <jignesh.rawal@alight.com>
2018-04-02 21:48:46 +0530

    Minor changes

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from '../models/user.model';
-import { Http, Response, RequestOptions, Headers, ResponseType } from '@angular/http';
+
 @Injectable()
 export class LoginService {
 
     constructor(private http: HttpClient) { }
 
     validateLogin(user: User) {
-        let header = new HttpHeaders();
 
         const httpOptions = {
             headers: new HttpHeaders().set('content-type', 'application/json')
         };
-        let other_header = httpOptions.headers.set('content-type', 'application/json; charset=utf-8');
+
+       // let other_header = httpOptions.headers.set('content-type', 'application/json; charset=utf-8');
 
         return this.http.post('http://localhost:3005/api/user/login', {
             username: user.username,
