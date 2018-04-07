2ae2bd90ce73749d77e9c8e394e52412b8c3a4b6
JigneshRaval <jigneshraval2009@gmail.com>
2018-04-07 14:58:20 +0530

    updated index.ts

--- src/app1-evanto-blog/components/index.ts
+++ src/app1-evanto-blog/components/index.ts
@@ -3,27 +3,9 @@ import { NgTemplateNgForComponent } from './ng-templates-examples/ng-template-ng
 // Export all Components, Entry Components, Directives, Pipes and Services
 export const MY_EXAMPLES = {
     "COMPONENTS" : [
-        HeaderComponent,
-		JumbotronComponent,
-		AlbumCardComponent,
-		FooterComponent,
-		BootstrapTemplateComponent,
-		Example1TabComponent,
-		Example1TabContentComponent,
-		Example1TabMainComponent,
-		CarouselComponent,
-		Example3CarouselMainComponent,
-		DialogComponent,
-		AdBannerComponent,
-        AdDirective,
-        HeroJobAdComponent,
-        HeroProfileComponent,
-        Example2MainComponent,
-        TemplateDrivenForm1Component,
-        LoginComponent
     ],
-    "ENTRY_COMPONENTS": [HeroJobAdComponent, HeroProfileComponent, DialogComponent],
-    "DIRECTIVES": [DialogAnchorDirective],
+    "ENTRY_COMPONENTS": [],
+    "DIRECTIVES": [],
     "PIPES": [],
-    "SERVICES" : [AdService, PageVisibilityService]
+    "SERVICES" : []
 }
