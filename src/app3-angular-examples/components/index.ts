9fb38c75268717d0cc9a4b9cc0e30362c2ec3dec
Jignesh Raval <jignesh.rawal@alight.com>
2018-04-09 22:09:25 +0530

    UI imporvement for Left navigation and Content part

--- src/app3-angular-examples/components/index.ts
+++ src/app3-angular-examples/components/index.ts
@@ -3,12 +3,18 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'examples-sidebar',
     template: `
-    <nav class="examples-sidebar">
-        <a class="p-2 text-dark" routerLink="/">Features</a>
-        <a class="p-2 text-dark" routerLink="/home">Enterprise</a>
-        <a class="p-2 text-dark" routerLink="/adbanner">Ad Banner</a>
-        <a class="p-2 text-dark" routerLink="/contact">contact us</a>
-    </nav>
+        <nav class="bd-links examples-sidebar" id="bd-docs-nav">
+            <div class="bd-toc-item">
+                <a class="bd-toc-link" href="/docs/4.0/getting-started/introduction/">Getting started</a>
+
+                <ul class="bd-sidenav">
+                    <li><a routerLink="/">Introduction</a></li>
+                    <li><a routerLink="/home">Download</a></li>
+                    <li><a routerLink="/adbanner">Ad Banner</a></li><li>
+                    <li><a class="p-2 text-dark" routerLink="/contact">contact us</a></li>
+                </ul>
+            </div>
+        </nav>
     `
 })
 export class ExamplesSidebarComponent {
