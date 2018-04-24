b1ff899a3e0b00cc7ba81d4bfe1d4b2a32f3d174
Jignesh Raval <jignesh.rawal@alight.com>
2018-04-24 21:17:10 +0530

    Added function to Highlight code on Every Router change.

--- src/app3-angular-examples/app.component.ts
+++ src/app3-angular-examples/app.component.ts
@@ -1,9 +1,12 @@
 import { Component, ViewChild } from '@angular/core';
 
+/* declare var jquery: any;
+declare var $: any;
+declare var hljs: any; */
+
 @Component({
     selector: 'app-root',
     template: `
-
         <div class="uk-container uk-container-expand">
             <header-component></header-component>
 
@@ -12,17 +15,25 @@ import { Component, ViewChild } from '@angular/core';
                     <examples-sidebar></examples-sidebar>
                 </div>
                 <div class="uk-width-1-1 uk-padding">
-                    <router-outlet></router-outlet>
+                    <router-outlet (activate)="changeOfRoutes()"></router-outlet>
                 </div>
             </div>
 
             <footer-component></footer-component>
         </div>
-
-
-
     `
 })
 export class AppComponent {
 
+    changeOfRoutes() {
+        alert('Hi router changed');
+        // Highlight code syntax
+        if ($) {
+            $('pre code').each(function (i, block) {
+                if (hljs) {
+                    hljs.highlightBlock(block);
+                }
+            });
+        }
+    }
 }
